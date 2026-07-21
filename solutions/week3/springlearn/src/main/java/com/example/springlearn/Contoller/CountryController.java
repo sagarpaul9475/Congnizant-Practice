04923e62f8555a1b33b7b9e22a4e603f244a8672
sagarpaul9475 <sagarpaul9475@gmail.com>
2026-07-21 15:16:26 +0530

    Add CountryService class; implement getCountry method to retrieve country details by code and update CountryController to use it

--- solutions/week3/springlearn/src/main/java/com/example/springlearn/Contoller/CountryController.java
+++ solutions/week3/springlearn/src/main/java/com/example/springlearn/Contoller/CountryController.java
@@ -1,20 +1,25 @@
 package com.example.springlearn.Contoller;
 
 import com.example.springlearn.model.Country;
+import com.example.springlearn.service.CountryService;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CountryController {
+    @Autowired
+    private CountryService countryService;
 
     private static final Logger LOGGER =
             LoggerFactory.getLogger(CountryController.class);
@@ -34,6 +39,11 @@ public class CountryController {
         ((ClassPathXmlApplicationContext) context).close();
 
         return country;
+    }
+
+    @GetMapping("/countries/{code}")
+    public Country getCountry(@PathVariable String code) {
+        return countryService.getCountry(code);
     }
     @GetMapping("/countries")
     public List<Country> getAllCountries() {
